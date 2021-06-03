4ba06090c54718585ffda5d5c24db1c34691a8a3
wiolGit <85089238+wiolGit@users.noreply.github.com>
2021-06-03 23:15:16 +0200

    Add files via upload

---
+++ projecta/projecta/src/test/java/com/example/projecta/ProjectaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projecta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
